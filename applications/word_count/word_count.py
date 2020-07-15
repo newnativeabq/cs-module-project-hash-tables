8c7da692ac357cd13980375a5418970e973b5b06
newnativeabq <vincent.a.brandon@gmail.com>
2020-07-14 22:13:08 -0600

    ENH: beefed up word splitter to look for return stuffs

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,53 @@
-def word_count(s):
-    # Your code here
+import re
+
+def read_file(path):
+    with open(path, 'r') as file:
+        t = file.read()
+    return t
 
 
+def _update_dict(dictionary, key, val):
+    try:
+        dictionary[key] += val
+    except:
+        if (key is not None) and (len(key)>0):
+            dictionary[key] = val
+
+
+def _valid_char(char):
+    if char not in '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split():
+        return char
+
+
+def _coalesce(*args):
+    for i in args:
+        if i is not None:
+            return i
+
+
+def _clean_word(word):
+    word = word.lower()
+    cword = ''.join([_coalesce(_valid_char(c), '') for c in word])
+    return cword
+
+
+def split_string(string):
+    items = []
+    for item in string.split():
+        for sub in item.split():
+            items.append(sub)
+    return items
+
+
+def word_count(string):
+    words = {}
+    count = 0
+
+    for word in split_string(string):
+        cword = _clean_word(word)
+        _update_dict(words, cword, 1)
+
+    return words
 
 if __name__ == "__main__":
     print(word_count(""))
